37495b9573b447022825beb4185ffd9f699a9bf1
Loxphordex <silasishallahan@gmail.com>
2019-06-29 11:32:26 -0700

    nav stuff

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <nav role='navigation' className='main-nav'>
+        <h1>Molly Reusser</h1>
+        <ul>
+          <li><Link to='/shop'>Shop</Link></li>
+          <li><Link to='/gallery'>Gallery</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
+        </ul>
+      </nav>
+    )
+  }
+}
