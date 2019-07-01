bc693e15ccf5800e8c850181127be9ccf1ef91e3
Loxphordex <silasishallahan@gmail.com>
2019-07-01 16:37:37 -0700

    clickable gallery

--- src/routes/Gallery/Gallery.js
+++ src/routes/Gallery/Gallery.js
@@ -7,6 +7,9 @@ export default class Gallery extends React.Component {
   state = {
     index: 1,
     images: null,
+    fullScreen: false,
+    fullScreenImage: null,
+    galleryDisabled: '',
   }
 
   componentDidMount() {
@@ -18,7 +21,9 @@ export default class Gallery extends React.Component {
     // So we take the index and multiply it by 12 (1*12=12, 2*12=24, etc...)
     // We want the range of images to be between this new value (12)
     // and the new value minus 12, so we'll have a range of 1 to 12
+
     const { index } = this.state
+
     const imagesDisplayed = index * 12;
     const images = galleryImages.slice(imagesDisplayed - 12, imagesDisplayed)
     console.log('iMAGE LENGTH: ', images.length )
@@ -31,7 +36,10 @@ export default class Gallery extends React.Component {
 
     return images.map(image => {
       return (
-        <div className={`gallery-image ${image.name}`} key={image.name}>
+        <div 
+          className={`gallery-image ${image.name}`} 
+          key={image.name}
+          onClick={(event) => this.handleFullScreen(event.target.src)}>
           <Image publicId={image.url} />
           <h2>{image.name}</h2>
         </div>
@@ -39,16 +47,62 @@ export default class Gallery extends React.Component {
     })
   }
 
+  handleFullScreen = (url) => {
+    this.setState({ 
+      fullScreen: true,
+      fullScreenImage: url,
+      galleryDisabled: 'gallery-disabled',
+    })
+  }
+
+  handleDisableFullScreen = () => {
+    this.setState({
+      fullScreen: false,
+      fullScreenImage: null,
+      galleryDisabled: '',
+    })
+  }
+
+  handleNext = async() => {
+    const { index, images } = this.state
+    if (!!images && images.length === 12) {
+      await this.setState({ index: index+1 })
+      this.setDisplayedImages()
+    }
+  }
+
+  handlePrevious = async() => {
+    const { index } = this.state
+    if (index > 1) {
+      await this.setState({ index: index-1 })
+      this.setDisplayedImages()
+    }
+  }
+
   render() {
-    const { images } = this.state
+    const { images, fullScreen, fullScreenImage, galleryDisabled } = this.state
 
     return(
       <section className='gallery-wrapper'>
-        <div className='image-area'>
+
+        { !!fullScreen && 
+        <div className='fullscreen-background' onClick={() => this.handleDisableFullScreen()}>
+          <CloudinaryContext cloudName='dghqlm5xb'>
+            <Image publicId={fullScreenImage} />
+          </CloudinaryContext>
+        </div> }
+
+        <div className={`image-area ${galleryDisabled}`}>
           <CloudinaryContext cloudName='dghqlm5xb'>
             { !!images && this.createImageElements() }
           </CloudinaryContext>
-        </div>
+        </div> 
+
+        <div className={`prev-next-area ${galleryDisabled}`}>
+          <button className='gallery-prev' onClick={() => this.handlePrevious()}>{'<'}</button>
+          <button className='gallery-next' onClick={() => this.handleNext()}>{'>'}</button>
+        </div> 
+        
       </section>
     )
   }
