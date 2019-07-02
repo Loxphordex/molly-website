14aef7a4f2b48dab94ef23e14e900933f6a2ca55
Loxphordex <silasishallahan@gmail.com>
2019-07-01 22:13:00 -0700

    image sized down

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,8 +8,10 @@ export default class Header extends React.Component {
       <nav role='navigation' className='main-nav'>
         <h1> <Link to='/'>Art by Molly Reusser</Link> </h1>
         <ul>
-          <li><a href='https://www.etsy.com/shop/GolgiGoods' 
-            target='_blank' rel='noopener noreferrer'>Shop</a></li>
+          <li>
+            <a href='https://www.etsy.com/shop/GolgiGoods' 
+            target='_blank' rel='noopener noreferrer'>Shop</a>
+          </li>
           <li><Link to='/gallery'>Gallery</Link></li>
           <li><Link to='/about'>About</Link></li>
           <li><Link to='/contact'>Contact</Link></li>
