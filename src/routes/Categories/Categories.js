bfb181930dca517e3ea6a2d90ec0586899be9913
Loxphordex <silasishallahan@gmail.com>
2019-07-04 15:06:24 -0700

    changed font

--- src/routes/Categories/Categories.js
+++ src/routes/Categories/Categories.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Categories.css'
 
 export default class Categories extends React.Component {
   render() {
     return(
       <section className='categories-wrapper'>
-        <div><Link to='/gallery/demo'>Demo</Link></div>
-        <div><Link to='/gallery/graphite'>Graphite</Link></div>
+        <div className='categories-area'>
+          <div className='category c-demo'><Link to='/gallery/demo'>Demo</Link></div>
+          <div className='category c-graphite'><Link to='/gallery/graphite'>Graphite</Link></div>
+          <div className='category c-sculpture'><Link to='/gallery/sculpture'>Sculpture</Link></div>
+          <div className='category c-pixel'><Link to='/gallery/pixel'>Pixel</Link></div>
+
+        </div>
       </section>
     )
   }
