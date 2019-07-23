0609f5edf327a2ea11e118a859baaaaab34d116e
Silas Hallahan <silasishallahan@gmail.com>
2019-07-23 14:35:51 -0700

    merging

--- src/routes/Categories/Categories.js
+++ src/routes/Categories/Categories.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Image, Transformation, CloudinaryContext } from 'cloudinary-react'
+import images from '../../images/category-images'
 import './Categories.css'
 
 export default class Categories extends React.Component {
@@ -9,10 +11,39 @@ export default class Categories extends React.Component {
     return(
       <section className='categories-wrapper'>
         <div className='categories-area'>
-          <div className='category c-pencil'><Link to='/gallery/pencil'>Pencil</Link></div>
-          <div className='category c-pendandink'><Link to='/gallery/penandink'>{'Pen & Ink'}</Link></div>
-          <div className='category c-sculpture'><Link to='/gallery/sculpture'>Sculpture</Link></div>
-          <div className='category c-digital'><Link to='/gallery/digital'>Digital</Link></div>
+
+          <CloudinaryContext cloudName='dghqlm5xb'>
+            <div className='category c-pencil'>
+              <Image publicId={images.pencil} type='fetch' name='pencil'>
+                <Transformation quality="60" width="300" crop="scale" />
+              </Image>
+              <Link to='/gallery/pencil'>Pencil</Link>
+            </div>
+
+
+            <div className='category c-pendandink'>
+              <Image publicId={images.penandink} type='fetch' name='penandink'>
+                <Transformation quality="60" width="300" crop="scale" />
+              </Image>
+              <Link to='/gallery/penandink'>{'Pen & Ink'}</Link>
+            </div>
+
+
+            <div className='category c-sculpture'>
+              <Image publicId={images.sculpture} type='fetch' name='sculpture'>
+                <Transformation quality="60" width="300" crop="scale" />
+              </Image>
+              <Link to='/gallery/sculpture'>Sculpture</Link>
+            </div>
+
+
+            <div className='category c-digital'>
+              <Image publicId={images.digital} type='fetch' name='digital'>
+                <Transformation quality="60" width="300" crop="scale" />
+              </Image>
+              <Link to='/gallery/digital'>Digital</Link>
+            </div>
+          </CloudinaryContext>
 
         </div>
       </section>
